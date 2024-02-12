2357bc19e581dec3f19ee81390c94f903f7b3b95
XinLian132243 <156954716+XinLian132243@users.noreply.github.com>
2024-02-12 23:06:13 +0800

    Create BC_EnhancedEedia.user.js

---
+++ BC_EnhancedEedia.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         BC 聊天室扩展
+// @namespace    https://www.bondageprojects.com/
+// @version      0.1.1
+// @description  聊天室扩展
+// @author       XinLian
+// @include      /^https:\/\/(www\.)?bondageprojects\.elementfx\.com\/R\d+\/(BondageClub|\d+)(\/((index|\d+)\.html)?)?$/
+// @include      /^https:\/\/(www\.)?bondage-europe\.com\/R\d+\/(BondageClub|\d+)(\/((index|\d+)\.html)?)?$/
+// @connect      github.com
+// @grant        GM_xmlhttpRequest
+// @license      MIT
+// @run-at       document-end
+// ==/UserScript==
+
+(function () {
+    'use strict';
+	console.log("[ChatRoomEx] Start Load");
+    const script = document.createElement("script");
+    const timestamp = new Date().getTime(); // 创建当前时间的时间戳
+    script.src = `https://xinlian132243.github.io/BCMod/Source/BC_ChatRoomExSource.user.js?timestamp=${timestamp}`;
+    document.head.appendChild(script);
+
+})();
+
